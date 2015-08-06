d63b5e85cf2c2767272050ffe43b2bafdbd4a0ef
Capibara- <chubi123@gmail.com>
2015-08-06 13:40:38 +0300

    Moved function to class.

--- ShodanSearcher.py
+++ ShodanSearcher.py
@@ -34,17 +34,6 @@ def main(argv):
         print "\n[+] Matching IP addresses that return HTTP %d:" % http_code
         print "\n".join(results[http_code])
 
-def get_status_code(host, path="/", timeout=5, verbose=False):
-    try:
-        if verbose:
-            print "[+] Sending GET request to %s." % host
-        r = requests.get("http://%s%s" % (host.encode('ascii', 'ignore'), path.encode('ascii', 'ignore')),
-                         timeout=timeout)
-        return r.status_code
-    except (requests.ConnectionError, requests.exceptions.ConnectTimeout) as e:
-        if verbose:
-            print "[+] Bad reply from %s: %s." % (host, str(e))
-        return None
 
 
 class ShodanSearcher:
@@ -81,7 +70,7 @@ class ShodanSearcher:
             print "[+] Error while querying Shodan: %s." % e
 
         print "[+] Got %s results from Shodan search, sending GET requests." % results['total']
-        live_results = map(lambda x : (x['ip_str'], get_status_code(x['ip_str'],
+        live_results = map(lambda x : (x['ip_str'], self._get_status_code(x['ip_str'],
                                                                timeout=self.timeout,
                                                                verbose=self.verbose)), results['matches'])
         output = defaultdict(list)
@@ -90,6 +79,19 @@ class ShodanSearcher:
             if http_code in self.http_codes:
                 output[http_code].append(ip)
         return output
+
+
+    def _get_status_code(self, host, path="/", timeout=5, verbose=False):
+        try:
+            if verbose:
+                print "[+] Sending GET request to %s." % host
+            r = requests.get("http://%s%s" % (host.encode('ascii', 'ignore'), path.encode('ascii', 'ignore')),
+                             timeout=timeout)
+            return r.status_code
+        except (requests.ConnectionError, requests.exceptions.ConnectTimeout) as e:
+            if verbose:
+                print "[+] Bad reply from %s: %s." % (host, str(e))
+            return None
 
 if __name__ == '__main__':
     main(sys.argv)
