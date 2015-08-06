5f958c404bb2e9fae8baccb82684a1bf4ce48bc5
Capibara- <chubi123@gmail.com>
2015-08-06 13:37:23 +0300

    Added ShodanSearcher class.

--- ShodanSearcher.py
+++ ShodanSearcher.py
@@ -1,9 +1,20 @@
 __author__ = 'Capibara'
 
+
+ #  ____    __                  __                   ____                                  __
+ # /\  _`\ /\ \                /\ \                 /\  _`\                               /\ \
+ # \ \,\L\_\ \ \___     ___    \_\ \     __      ___\ \,\L\_\     __     __     _ __   ___\ \ \___      __   _ __
+ #  \/_\__ \\ \  _ `\  / __`\  /'_` \  /'__`\  /' _ `\/_\__ \   /'__`\ /'__`\  /\`'__\/'___\ \  _ `\  /'__`\/\`'__\
+ #    /\ \L\ \ \ \ \ \/\ \L\ \/\ \L\ \/\ \L\.\_/\ \/\ \/\ \L\ \/\  __//\ \L\.\_\ \ \//\ \__/\ \ \ \ \/\  __/\ \ \/
+ #    \ `\____\ \_\ \_\ \____/\ \___,_\ \__/.\_\ \_\ \_\ `\____\ \____\ \__/.\_\\ \_\\ \____\\ \_\ \_\ \____\\ \_\
+ #     \/_____/\/_/\/_/\/___/  \/__,_ /\/__/\/_/\/_/\/_/\/_____/\/____/\/__/\/_/ \/_/ \/____/ \/_/\/_/\/____/ \/_/
+
+
 import shodan
 import sys
 import requests
 import argparse
+from collections import defaultdict
 
 SHODAN_API_KEY = ""
 
@@ -17,27 +28,11 @@ def main(argv):
 
 
     keywords = ' '.join(args.keywords)
-    search_term = "%s country:%s" % (keywords, args.country) if args.country else keywords
-    timeout = args.timeout if args.timeout else 5
-    api = shodan.Shodan(SHODAN_API_KEY)
-
-    try:
-        results = api.search(search_term)
-    except shodan.APIError, e:
-        print "[+] Error while querying Shodan: %s." % e
-
-    print "[+] Got %s results from Shodan search, sending GET requests." % results['total']
-
-    live_results = map(lambda x: (x['ip_str'], get_status_code(x['ip_str'],
-                                                               timeout=args.timeout,
-                                                               verbose=args.verbose)), results['matches'])
-
-    print "\n[+] Matching IP addresses that return HTTP 200:"
-    print "\n".join([x for (x, y) in filter(lambda (x, y): y == 200, live_results)])
-
-    print "\n[+] Matching IP addresses that return HTTP 401:"
-    print "\n".join([x for (x, y) in filter(lambda (x, y): y == 401, live_results)])
-
+    searcher = ShodanSearcher(keywords, country_code=args.country) if args.country else ShodanSearcher(keywords)
+    results = searcher.search()
+    for http_code in results:
+        print "\n[+] Matching IP addresses that return HTTP %d:" % http_code
+        print "\n".join(results[http_code])
 
 def get_status_code(host, path="/", timeout=5, verbose=False):
     try:
@@ -50,6 +45,51 @@ def get_status_code(host, path="/", timeout=5, verbose=False):
         if verbose:
             print "[+] Bad reply from %s: %s." % (host, str(e))
         return None
+
+
+class ShodanSearcher:
+    """A class to automate Shodan searches and filter results.
+        keywords        Self explanatory.
+        country_code    Optional.
+        timeout         Timeout for HTTP GET requests.
+        verbose         Verbose output.
+        http_codes      Optional, defaults to 200 and 401.
+    """
+    keywords = ""
+    country_code = None
+    timeout = 5
+    verbose = False
+    http_codes = None
+
+    def __init__(self, keywords, country_code=None, timeout=5, verbose=False, http_codes=[200, 401]):
+        self.keywords = keywords
+        self.country_code =  country_code
+        self.timeout = timeout
+        self.verbose = verbose
+        self.http_codes = http_codes
+
+    def search(self):
+        """Searches ShodanHQ.io for the given keywords then filters out results that do not respond with the specified HTTP code.
+        Returns a dictionary mapping the HTTP code to a list of IPs that returned that code.
+        """
+        search_term = "%s country:%s" % (self.keywords, self.country_code) if self.country_code else self.keywords
+        api = shodan.Shodan(SHODAN_API_KEY)
+
+        try:
+            results = api.search(search_term)
+        except shodan.APIError, e:
+            print "[+] Error while querying Shodan: %s." % e
+
+        print "[+] Got %s results from Shodan search, sending GET requests." % results['total']
+        live_results = map(lambda x : (x['ip_str'], get_status_code(x['ip_str'],
+                                                               timeout=self.timeout,
+                                                               verbose=self.verbose)), results['matches'])
+        output = defaultdict(list)
+
+        for (ip, http_code) in live_results:
+            if http_code in self.http_codes:
+                output[http_code].append(ip)
+        return output
 
 if __name__ == '__main__':
     main(sys.argv)
